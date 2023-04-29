ae302c56fb3aeab4c7bd438c1c4a023732c82c79
dany0814 <jcanahuirev@gmail.com>
2023-04-28 22:37:35 -0500

    fix: clean Delete method

--- internal/platform/server/server.go
+++ internal/platform/server/server.go
@@ -71,7 +71,7 @@ func (s *Server) registerRoutes() {
 func (s *Server) setCors() {
 	s.engine.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000/,http://127.0.0.1:3000/,http://0.0.0.0:3000/"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "HEAD", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "application/json", "text/plain; charset=utf-8"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
