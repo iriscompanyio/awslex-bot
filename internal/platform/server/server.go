cd1104a12f8f5aa73da2ca0b71114c21c58325f5
dany0814 <jcanahuirev@gmail.com>
2023-04-28 20:41:23 -0500

    fix: set config cors

--- internal/platform/server/server.go
+++ internal/platform/server/server.go
@@ -69,5 +69,12 @@ func (s *Server) registerRoutes() {
 }
 
 func (s *Server) setCors() {
-	s.engine.Use(cors.Default())
+	s.engine.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"PUT", "PATCH", "POST"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 }
